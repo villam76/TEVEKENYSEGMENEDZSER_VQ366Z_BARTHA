0e7a23c0afee1480a68da40b47aede0c487e1b2a
villam76 <b.sebi02@gmail.com>
2025-03-30 18:36:13 +0200

    controller cs created with api endpoint

---
+++ BACKEND/Controllers/TimeManagerController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("api/timemanager")]
+    public class TimeManagerController : ControllerBase
+    {
+    }
+}
