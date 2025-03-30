74e1bbc7ed19f0fa497cc8084c55b20568be7b71
villam76 <b.sebi02@gmail.com>
2025-03-30 21:45:22 +0200

    Added ScheduledTask initializer to method
    also added a placeholder while-cycle for further use

--- BACKEND/Controllers/TimeManagerController.cs
+++ BACKEND/Controllers/TimeManagerController.cs
@@ -1,5 +1,7 @@
 ﻿using BACKEND.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace BACKEND.Controllers
 {
@@ -16,7 +18,31 @@ namespace BACKEND.Controllers
 
         private List<TimeManagerModels.ScheduledTask> ScheduleCreator(List<TimeManagerModels.TaskItem> items, int daysToComplete)
         {
-            throw new NotImplementedException();
+            List<TimeManagerModels.ScheduledTask> schedule = new List<TimeManagerModels.ScheduledTask>();
+
+            List<TimeManagerModels.TaskItem> sortedItems = items.OrderBy(x => x.Duration).ToList();
+
+            int currentDay = 1;
+            int currentHour = 7; // 8-tól 17-ig tart a munkaidő, mivel mindig van egy óra szünet a feladatok között
+
+            for (int i = 0; i < sortedItems.Count; i++)
+            {
+                while (true)
+                {
+                    break; //TODO!!!
+                }
+                // Ütemezett feladat létrehozása
+                schedule.Add(new TimeManagerModels.ScheduledTask
+                {
+                    Name = sortedItems[i].Name,
+                    Day = currentDay,
+                    StartHour = currentHour + 1,
+                    EndHour = currentHour + sortedItems[i].Duration + 1
+                });
+
+            }
+
+            return schedule;
         }
     }
 }
