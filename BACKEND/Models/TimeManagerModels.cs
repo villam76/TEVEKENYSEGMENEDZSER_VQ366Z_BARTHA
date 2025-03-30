c9b9b6b946a39cc3b65d13bd611fa960cccd9efb
villam76 <b.sebi02@gmail.com>
2025-03-30 16:11:29 +0200

    TimeManager model created

---
+++ BACKEND/Models/TimeManagerModels.cs
@@ -0,0 +1,25 @@
+﻿namespace BACKEND.Models
+{
+    public class TimeManagerModels
+    {
+        public class TaskItem
+        {
+            public string Name { get; set; } = String.Empty;
+            public int Duration {  get; set; }
+        }
+
+        public class ScheduleRequest
+        {
+            public List<TaskItem> Items { get; set; } = new List<TaskItem>();
+            public int DaysToComplete { get; set; }
+        }
+
+        public class ScheduledTask
+        {
+            public string Name { get; set; } = String.Empty;
+            public int Day { get; set; }
+            public int StartHour {  get; set; }
+            public int EndHour { get; set; }
+        }
+    }
+}
